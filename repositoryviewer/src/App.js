02ccd4577e3b25524d61301204462d6df8b36a05
EvgeniyRybchuk <jeka.rubchuk@gmail.com>
2021-11-05 00:02:17 +0300

    fix merge

--- repositoryviewer/src/App.js
+++ repositoryviewer/src/App.js
@@ -1,10 +1,12 @@
 import './styles/App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import Header from "./components/header";
 
 function App() {
   return (
     <div className="App">
+        <Header />
       <FontAwesomeIcon icon={faCoffee} />
     </div> 
   );
