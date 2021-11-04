20e651d9cd18f4701f8890269c59ae70109d0556
EvgeniyRybchuk <jeka.rubchuk@gmail.com>
2021-11-04 21:58:13 +0300

    front ready

--- docs/assets/script/script.js
+++ docs/assets/script/script.js
@@ -117,3 +117,78 @@ document.body.addEventListener("click", (e) =>
     if(e.target.className != "path-history" && e.target.className != "fas fa-caret-down")
         pathList.classList.remove("open"); 
 })
+
+
+
+// let testdata = [
+//     { path: '.browserslistrc'},
+//     { path: '.editorconfig'},
+//     { path: '.gitignore'},
+//     { path: 'README.md'}, 
+//     { path: 'angular.json'},
+//     { path: 'calc_layout'},
+//     { path: 'calc_layout/index.html'},
+//     { path: 'calc_layout/style.css'},
+//     { path: 'karma.conf.js'},
+//     { path: 'package-lock.json'},
+//     { path: 'package.json'},
+//     { path: 'src'},
+//     { path: 'src/app'},
+//     { path: 'src/app/app.component.css'},
+//     { path: 'src/app/app.component.html'},
+//     { path: 'src/app/app.component.ts'},
+//     { path: 'src/app/app.module.ts'},
+//     { path: 'src/assets'},
+//     { path: 'src/assets/.gitkeep'},
+//     { path: 'src/environments'},
+//     { path: 'src/environments/environment.prod.ts'},
+//     { path: 'src/environments/environment.ts'},
+//     { path: 'src/favicon.ico'},
+//     { path: 'src/index.html'},
+//     { path: 'src/main.ts'},
+//     { path: 'src/polyfills.ts'},
+//     { path: 'src/styles.css'}, 
+//     { path: 'src/test.ts'},
+//     { path: 'tsconfig.app.json'},
+//     { path: 'tsconfig.json'},
+//     { path: 'tsconfig.spec.json'}
+// ]; 
+
+let repos = []; 
+let curRepos = { }; 
+
+// дополнение информации о файле/папке
+function pathDataModify(obj)
+{
+    let pathArray = obj.path.split("/");
+    obj.name = pathArray.pop();  
+    obj.owner = pathArray.join("/"); 
+}
+// получение контента определённой папки репозитория по указаному пути  
+function openFolder(repos, path)
+{
+    return repos.tree.filter((e) => { 
+        return e.owner === path ? true : false 
+     })
+}
+// получение всех данных репозитория  
+async function getFiles(reposName) { 
+    const i = await fetch(`https://api.github.com/repos/EvgeniyRyabchuk/${reposName}/git/trees/master?recursive=1`)
+    const data = await i.json(); 
+    for(let i of data.tree) 
+        pathDataModify(i); 
+    
+    curRepos = data;
+    curRepos.name = reposName; 
+} 
+// пполучение всех репозиториев 
+async function getRepos(path) { 
+    const i = await fetch(path) 
+    const data = await i.json(); 
+    repos = data; 
+}
+
+getRepos("https://api.github.com/users/EvgeniyRyabchuk/repos");
+getFiles("calc");  
+
+ 
