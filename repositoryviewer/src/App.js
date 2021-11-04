cc288787f126b14f2441160843810ecc69e77797
EvgeniyRybchuk <jeka.rubchuk@gmail.com>
2021-11-04 23:23:54 +0300

    get started react app

---
+++ repositoryviewer/src/App.js
@@ -0,0 +1,13 @@
+import './styles/App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+function App() {
+  return (
+    <div className="App">
+      <FontAwesomeIcon icon={faCoffee} />
+    </div> 
+  );
+}
+
+export default App;
