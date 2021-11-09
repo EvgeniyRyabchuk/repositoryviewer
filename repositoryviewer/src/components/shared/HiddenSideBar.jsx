d5ed9fe075cc39eb89b94820ba8be573d73f04a6
EvgeniyRybchuk <jeka.rubchuk@gmail.com>
2021-11-09 11:24:50 +0300

    add current user display in hidden sidebar

--- repositoryviewer/src/components/shared/HiddenSideBar.jsx
+++ repositoryviewer/src/components/shared/HiddenSideBar.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {useHistory, useLocation} from "react-router-dom";
+import {UserContext} from "../../context";
 
 const HiddenSideBar = ({isOpen, openHiddenSideBar}) => {
+    const {user, serUser} = useContext(UserContext);
+
     const history = useHistory();
     const location = useLocation();
     const sidebarLinks = [
@@ -13,19 +16,19 @@ const HiddenSideBar = ({isOpen, openHiddenSideBar}) => {
         history.push(`/${e}`);
         openHiddenSideBar(false); 
     }
-
+    console.log(user);
     return (
         <div>
             <div onClick={() => openHiddenSideBar(false)} className={ isOpen === true ? 'side_wrapper side_wrapper--show' : 'side_wrapper'}></div>
             <div className={ isOpen === true ? 'hidden-aside hidden-aside--show' : 'hidden-aside'}>
                 <div className="profile">
                     <div className="profile__img__wrapper">
-                        <img src="https://www.tenforums.com/geek/gars/images/2/types/thumb_15951118880user.png" alt=""
+                        <img src={user ? user.avatar_url : 'https://www.tenforums.com/geek/gars/images/2/types/thumb_15951118880user.png'} alt=""
                              width="50" height="50" />
                     </div>
                     <span className="email">
-                    example.layout@gmail.com
-                </span>
+                        {user ? user.email ? user.email : user.username : ''}
+                    </span>
                 </div>
                 <div className="nav flex-column nav-pills mx-3" id="v-pills-tab" role="tablist"
                      aria-orientation="vertical">
